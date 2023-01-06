6612811716c6491eef10fd01c69f74d968b3a456
Gentrit Uka <gentrit-uka@live.com>
2023-01-06 16:43:37 +0100

    updated styling

--- space-project-attributy-frontend/src/App.tsx
+++ space-project-attributy-frontend/src/App.tsx
@@ -1,21 +1,21 @@
-import { useState } from 'react'
-import logo from './logo.svg'
+
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
 import Home from './components/Home'
 import MissionDetails from './components/MissionDetails'
+import Header from './components/Header'
 
 function App() {
   
 
   return (
     <div className="App">
-      
-    <Routes>
-      <Route index element={<Home />} />
-      <Route path="/home" element={<Home />} />
-      <Route path='/missiondetails' element={<MissionDetails />} />
-    </Routes>
+      <Header />
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path='/missiondetails' element={<MissionDetails />} />
+      </Routes>
 
     </div>
   )
---
+++ space-project-attributy-frontend/src/components/Header.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+function Header() {
+  return (
+    <header>
+        <img src='../src/assets/spacex.svg' width={350}/>
+        <div className='route-names'>
+            <NavLink className="route-names-item" to='/home'>Home</NavLink>
+        </div>
+    </header>
+  )
+}
+
+export default Header
